d0ffbab8cc7c4768de8a3083ed4f99aab5936f1e
jianghang.loujh <jianghang.loujh@alibaba-inc.com>
2023-03-27 14:04:00 +0800

    modify entrypoint remove jps

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -17,7 +17,7 @@
 POLARDBX_SQL_HOME=/home/admin/drds-server
 
 function polardbx_sql_pid() {
-  jps | grep Tddl | cut -d ' ' -f 1
+  ps auxf | grep java | grep TddlLauncher | awk '{print $2}'
 }
 
 function kill_and_clean() {
